7fc5a17bf2400007d8c057fb6cb35da5d721ec5b
HINA KHADIM <hinakhadim2002@gmail.com>
2020-04-15 13:30:48 +0500

    password border has some problems

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ let input=document.querySelectorAll("input");
 let label=document.querySelectorAll('label');
 let invalidEmail=document.querySelector('.invalid');
 // let password=document.querySelector('');
-
+let box=document.querySelector('.password');
 
 // for auto focus on webpage load after 10ms
 setTimeout(function(){
@@ -21,11 +21,14 @@ input[0].addEventListener('focusout', function(){
 
 input[1].addEventListener('focus', function(){
     label[1].className='shrink';
+    box.style.border='2px solid #1a73e8';
+    
 });
 
 // on focus out
 input[1].addEventListener('focusout', function(){
     label[1].classList.remove('shrink');
+    box.style.border='1px solid lightgrey';
 });
 // change warning a/c to situation 
 
