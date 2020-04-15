ad5fef37b40c073ceb9be95a9fa8ed3091476abe
HINA KHADIM <hinakhadim2002@gmail.com>
2020-04-15 12:31:04 +0500

    password input update

--- script.js
+++ script.js
@@ -1,17 +1,32 @@
-let input=document.querySelector("input");
-let label=document.querySelector('label');
+let input=document.querySelectorAll("input");
+let label=document.querySelectorAll('label');
+let invalidEmail=document.querySelector('.invalid');
+// let password=document.querySelector('');
+
 
 // for auto focus on webpage load after 10ms
 setTimeout(function(){
-    input.focus();
+    input[0].focus();
 },10)
 
-// on focus
-input.addEventListener('focus', function(){
-    label.className='shrink';
+input[0].addEventListener('focus', function(){
+    label[0].className='shrink';
 });
 
 // on focus out
-input.addEventListener('focusout', function(){
-    label.classList.remove('shrink');
+input[0].addEventListener('focusout', function(){
+    label[0].classList.remove('shrink');
 });
+
+
+input[1].addEventListener('focus', function(){
+    label[1].className='shrink';
+});
+
+// on focus out
+input[1].addEventListener('focusout', function(){
+    label[1].classList.remove('shrink');
+});
+// change warning a/c to situation 
+
+
